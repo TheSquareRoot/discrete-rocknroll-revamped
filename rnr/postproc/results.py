5830eca9b81402e8bb42283b20764160196797d3
Victor Bourgin <victor.bourgin1@universite-paris-saclay.fr>
2025-03-17 19:33:14 +0100

    Revamped results plotting abilities.

--- rnr/postproc/results.py
+++ rnr/postproc/results.py
@@ -39,6 +39,27 @@ class Results:
     def instant_rate(self) -> NDArray[np.floating]:
         return self.remaining_fraction[:-1] - self.remaining_fraction[1:]
 
+    def time_to_fraction(self, fraction: float) -> float:
+        # Normalize resuspended fraction by the final value
+        normalized_resuspended = self.resuspended_fraction / self.resuspended_fraction[-1]
+        print(self.resuspended_fraction[-1])
+
+        # Find the index where the fraction first exceeds the target percentage
+        idx = np.searchsorted(normalized_resuspended, fraction)
+
+        if idx == 0:
+            return self.time[0]  # If the target is reached immediately
+
+        if idx >= len(self.time):
+            return self.time[-1]  # If the target is never reached
+
+        # Linear interpolation for better accuracy
+        t1, t2 = self.time[idx - 1], self.time[idx]
+        f1, f2 = normalized_resuspended[idx - 1], normalized_resuspended[idx]
+        t_target = t1 + (fraction - f1) * (t2 - t1) / (f2 - f1)
+
+        return t_target
+
     def plot_distribution(self, t: int,) -> None:
         plt.clf()
 
@@ -49,36 +70,57 @@ class Results:
         plt.savefig(f'figs/distribution_tstep={t}.png', dpi=300)
 
     def plot_remaining_fraction(self, scale: str='log',) -> None:
-        plt.clf()
+        fig, ax = plt.subplots(figsize=(6,4))
 
-        plt.plot(self.time, self.remaining_fraction)
+        ax.plot(self.time, self.remaining_fraction)
 
-        plt.xscale(scale)
+        ax.set_xscale(scale)
 
-        plt.savefig(f'figs/remaining_fraction.png', dpi=300)
+        fig.tight_layout()
+
+        fig.savefig(f'figs/remaining_fraction.png', dpi=300)
+        plt.close(fig)
 
     def plot_resuspended_fraction(self, scale: str='log',) -> None:
-        plt.clf()
+        fig, ax = plt.subplots(figsize=(6, 5))
 
-        plt.plot(self.time, self.resuspended_fraction)
+        ax.plot(self.time, self.resuspended_fraction, color='r',)
 
-        plt.xscale(scale)
-        plt.ylim(0.0, 1.1)
+        ax.axvline(x=self.time_to_fraction(0.5), ymax=0.5*self.resuspended_fraction[-1]/1.1, color='r', linestyle='-',)
+        ax.axvline(x=self.time_to_fraction(0.9), ymax=0.9*self.resuspended_fraction[-1]/1.1, color='r', linestyle='--',)
+        ax.axvline(x=self.time_to_fraction(0.99), ymax=0.99*self.resuspended_fraction[-1]/1.1, color='r',linestyle=':',)
 
-        plt.grid(axis='x', which='both')
-        plt.grid(axis='y', which='major')
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Resuspended fraction')
 
-        plt.savefig(f'figs/resuspended_fraction.png', dpi=300)
+        ax.set_xscale(scale)
+        ax.set_xlim(left=self.time[1],)
+        ax.set_ylim(0.0, 1.1)
+
+        ax.grid(axis='x', which='both')
+        ax.grid(axis='y', which='major')
+
+        fig.tight_layout()
+
+        fig.savefig(f'figs/resuspended_fraction.png', dpi=300)
+        plt.close(fig)
 
     def plot_instant_rate(self, scale: str = 'log',) -> None:
-        plt.clf()
+        fig, ax = plt.subplots(figsize=(6, 5))
 
-        plt.plot(self.time[:-1], self.instant_rate)
+        ax.plot(self.time[:-1], self.instant_rate, color='r',)
 
-        plt.xscale(scale)
-        plt.yscale('log')
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Resuspension rate')
 
-        plt.grid(axis='x', which='both')
-        plt.grid(axis='y', which='major')
+        ax.set_xscale(scale)
+        ax.set_yscale('log')
+        ax.set_xlim(self.time[1], self.time[-1])
 
-        plt.savefig(f'figs/instant_rate.png', dpi=300)
+        ax.grid(axis='x', which='both')
+        ax.grid(axis='y', which='major')
+
+        fig.tight_layout()
+
+        fig.savefig(f'figs/instant_rate.png', dpi=300)
+        plt.close(fig)
