528b0bd86475408a7af6b766b118c0284917f4ba
Victor Bourgin <victor.bourgin1@universite-paris-saclay.fr>
2025-03-19 15:57:15 +0100

    Added logging lines to the simulation class.

--- rnr/simulation/simulation.py
+++ rnr/simulation/simulation.py
@@ -24,12 +24,16 @@ class Simulation:
 
     def run(self,):
         # Build population array
+        logger.info('Building population array...')
         counts = np.zeros([self.flow.nsteps, self.size_distrib.nbins, self.adh_distrib.nbins])
         counts[0,:,:] = self.size_distrib.weights[:, None] * self.adh_distrib.weights
 
+        # Compute the rates for the whole simulation
+        logger.info('Computing rates...')
         rate = rocknroll_model(self.size_distrib, self.adh_distrib, self.flow)
         dt = self.flow.time[1] - self.flow.time[0]
 
+        logger.info('Entering time loop...')
         for t in range(self.flow.nsteps-1):
             counts[t+1,:,:] = np.maximum(counts[t,:,:] * (1 - rate[t,:,:] * dt), 0)
 
