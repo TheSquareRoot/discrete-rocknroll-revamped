d971f9c0fb0b6b02508f4db79a24b0024a7a3ff7
Victor Bourgin <victor.bourgin1@universite-paris-saclay.fr>
2025-03-28 12:42:00 +0100

    The parameters of aerodynamic model are now user-defined.

--- rnr/core/aeromodel.py
+++ rnr/core/aeromodel.py
@@ -31,9 +31,22 @@ class BaseAeroModel(AeroModel):
     Estimates lift, drag and the burst frequency for a particle attached to a flat surface in a shear flow.
     All formulas are based on the results of O'Neill (1968) and Hall (1988).
     """
-    def __init__(self, density: float, viscosity: float, **kwargs) -> None:
+    def __init__(self,
+                 density: float,
+                 viscosity: float,
+                 drag_coeff: float,
+                 drag_power: float,
+                 lift_coeff: float,
+                 lift_power: float,
+                 burst_coeff: float,
+                 **kwargs) -> None:
         self.density = density
         self.viscosity = viscosity
+        self.drag_coeff = drag_coeff
+        self.drag_power = drag_power
+        self.lift_coeff = lift_coeff
+        self.lift_power = lift_power
+        self.burst_coeff = burst_coeff
 
     def lift(self,
              velocity: NDArray[np.floating],
@@ -43,7 +56,7 @@ class BaseAeroModel(AeroModel):
         velocity = velocity.reshape(-1, 1)
         radii = radii.reshape(1, -1)
 
-        return 20.9 * self.density * (self.viscosity ** 2) * ((radii * velocity / self.viscosity) ** 2.31)
+        return self.lift_coeff * self.density * (self.viscosity ** 2) * ((radii * velocity / self.viscosity) ** self.lift_power)
 
     def drag(self,
              velocity: NDArray[np.floating],
@@ -53,10 +66,10 @@ class BaseAeroModel(AeroModel):
         velocity = velocity.reshape(-1, 1)
         radii = radii.reshape(1, -1)
 
-        return 32.0 * self.density * (self.viscosity ** 2) * ((radii * velocity / self.viscosity) ** 2)
+        return self.drag_coeff * self.density * (self.viscosity ** 2) * ((radii * velocity / self.viscosity) ** self.drag_power)
 
     def burst(self,
              velocity: NDArray[np.floating],
              ) -> NDArray[np.floating]:
-        return 0.00658 * (velocity ** 2) / self.viscosity
+        return self.burst_coeff * (velocity ** 2) / self.viscosity
 
