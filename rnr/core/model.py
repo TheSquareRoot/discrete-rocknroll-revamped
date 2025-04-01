3880dfafcbc9edb772b4f71a3e0a98d0c7c59c74
Victor Bourgin <victor.bourgin1@universite-paris-saclay.fr>
2025-04-01 17:22:39 +0200

    Minor variable renaming in the RnR rate function.

--- rnr/core/model.py
+++ rnr/core/model.py
@@ -43,25 +43,25 @@ class RocknRollModel(ResuspensionModel):
                             is computed an returned.
         """
         if t is None: # Sequential case
-            adh_tiled = np.tile(self.adh_distrib.fadh, (self.flow.nsteps, 1, 1))
-            aero_tiled = np.tile(self.flow.faero, (self.adh_distrib.nbins, 1, 1)).transpose(1, 2, 0)
+            fadh = np.tile(self.adh_distrib.fadh, (self.flow.nsteps, 1, 1))
+            faero = np.tile(self.flow.faero, (self.adh_distrib.nbins, 1, 1)).transpose(1, 2, 0)
             fluct_var = np.tile(self.flow.fluct_var, (self.adh_distrib.nbins, 1, 1)).transpose(1, 2, 0)
-            burst_tiled = np.tile(self.flow.burst[:, np.newaxis, np.newaxis],
+            burst = np.tile(self.flow.burst[:, np.newaxis, np.newaxis],
                                   (1, self.size_distrib.nbins, self.adh_distrib.nbins))
         else: # Vectorized case
-            adh_tiled = self.adh_distrib.fadh
-            aero_tiled = np.tile(self.flow.faero[t, :].reshape(-1, 1), (1, self.adh_distrib.nbins))
+            fadh = self.adh_distrib.fadh
+            faero = np.tile(self.flow.faero[t, :].reshape(-1, 1), (1, self.adh_distrib.nbins))
             fluct_var = np.tile(self.flow.fluct_var[t, :].reshape(-1, 1), (1, self.adh_distrib.nbins))
-            burst_tiled = self.flow.burst[t] * np.ones([self.size_distrib.nbins, self.adh_distrib.nbins])
+            burst = self.flow.burst[t] * np.ones([self.size_distrib.nbins, self.adh_distrib.nbins])
 
         # Compute the aerodynamic fluctuation at detachment, and the variance of force fluctations
-        fluct = adh_tiled - aero_tiled
+        fluct = fadh - faero
 
         # Compute the resuspension rate
-        rate = burst_tiled * np.exp(- (fluct ** 2) / (2 * fluct_var)) / (
+        rate = burst * np.exp(- (fluct ** 2) / (2 * fluct_var)) / (
                     0.5 * (1 + erf(fluct / np.sqrt(2 * fluct_var))))
 
         # Makes sure the rate is never superior to the burst frequency
-        rate = np.minimum(rate, burst_tiled)
+        rate = np.minimum(rate, burst)
 
         return rate
