3b01aa8a3d65d14f4619ea05bcaf170610a00e7f
Victor Bourgin <victor.bourgin@universite-paris-saclay.fr>
2025-04-02 13:46:55 +0200

    Non-tested implementation of the Non-Gaussian RnR model.

--- rnr/core/model.py
+++ rnr/core/model.py
@@ -68,4 +68,17 @@ class NonGaussianRocknRollModel(RocknRollModel):
     def compute_rate(burst: NDArray[np.floating],
                      fluct: NDArray[np.floating],
                      fluct_var: NDArray[np.floating]) -> NDArray[np.floating]:
-        pass
+
+        # Non-Gaussian distrib parameters
+        A1 = 1.812562
+        A2 = 1.463790
+        Bf = 0.343658
+
+        # Compute the resuspension rate
+        zdh = fluct / np.sqrt(fluct_var)
+        rate = (Bf * burst) * ((zdh + A1)/(A2 ** 2)) * np.exp(-0.5 * ((zdh + A1)/A2) ** 2) / (1 - np.exp(-0.5 * ((zdh + A1)/A2) ** 2))
+
+        # Makes sure the rate is never superior to the burst frequency
+        rate = np.minimum(rate, (0.5*burst/np.pi))
+
+        return rate
