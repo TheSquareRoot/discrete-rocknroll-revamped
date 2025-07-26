d20b00854b47a54f1487c010652eb572f309566c
Victor B. <victor.bourgin@universite-paris-saclay.fr>
2025-07-26 17:02:22 +0200

    Minor corrections (mostly ruff syntax rules).

--- rnr/utils/parameters.py
+++ rnr/utils/parameters.py
@@ -53,21 +53,15 @@ def check_config(config: dict) -> None:
     # ADHESION PARAMETERS
     # If biasi params are selected, user should not define custom median and scatter values
     if config["adhdistrib"]["dist_params"] == "biasi":
-        if (config["adhdistrib"]["medians"] is not None) or (
-            config["distribution"]["spreads"] is not None
-        ):
-            logger.warning(
-                "Biasi parameters selected. Custom median and scatter parameters will be ignored."
-            )
+        if (config["adhdistrib"]["medians"] is not None) or (config["distribution"]["spreads"] is not None):
+            logger.warning("Biasi parameters selected. Custom median and scatter parameters will be ignored.")
 
         if config["physics"]["adhesion_model"] == "Rabinovich":
             logger.error("Biasi parameters should only be used with the JKR model.")
 
     # SIMULATION PARAMETERS
     if config["simulation"]["duration"] < config["simulation"]["dt"]:
-        logger.error(
-            f"dt must be smaller than total duration. dt set to {config['simulation']['duration']:.2f}s"
-        )
+        logger.error(f"dt must be smaller than total duration. dt set to {config['simulation']['duration']:.2f}s")
         config["simulation"]["dt"] = config["simulation"]["duration"]
 
     if config["simulation"]["duration"] < config["simulation"]["acc_time"]:
